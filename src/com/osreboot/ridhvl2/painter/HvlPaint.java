6db0e7bc62f3a3a366e21296f95ac7c8351fb982
osreboot <calvin.16.weaver@gmail.com>
2018-04-12 19:17:56 -0700

    Updated HvlPaint, HvlQuad and added Gson dependency

--- src/com/osreboot/ridhvl2/painter/HvlPaint.java
+++ src/com/osreboot/ridhvl2/painter/HvlPaint.java
@@ -4,11 +4,14 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.opengl.Texture;
 
 //TODO implement HvlRenderFrame functionality
-//TODO update comments with proper formatting
 /**
- * A flexible texture that is applied when drawing polygons with {@linkplain HvlPainter}. Usable in three modes (COLOR,
- * TEXTURE, RENDERFRAME). HvlPainter will automatically analyze the mode and draw the texture with the
+ * A flexible texture that is applied when drawing polygons with {@linkplain HvlPainter}. Usable in three modes 
+ * (<code>COLOR, TEXTURE, RENDERFRAME</code>). HvlPainter will automatically analyze the mode and draw the texture with the
  * appropriate commands.
+ * 
+ * <p>
+ * 
+ * This class is intended to be statically imported.
  * 
  * @author os_reboot
  *
@@ -16,11 +19,13 @@ import org.newdawn.slick.opengl.Texture;
 public final class HvlPaint {
 
 	private static HvlPaint global;
-	
+
 	/**
 	 * Re-assigns the value of a global variable and returns that variable. This is an optimization 
 	 * technique that removes the need for users to store their own HvlPaint instances and allows for rapid
 	 * {@linkplain HvlPainter} draw calls with varying HvlPaint values.
+	 * 
+	 * <p>
 	 * 
 	 * NOTE: this references a volatile memory location and should only be used directly inside HvlPainter
 	 * draw calls!
@@ -33,11 +38,13 @@ public final class HvlPaint {
 		else global.setValue(cArg);
 		return global;
 	}
-	
+
 	/**
 	 * Re-assigns the value of a global variable and returns that variable. This is an optimization 
 	 * technique that removes the need for users to store their own HvlPaint instances and allows for rapid
 	 * {@linkplain HvlPainter} draw calls with varying HvlPaint values.
+	 * 
+	 * <p>
 	 * 
 	 * NOTE: this references a volatile memory location and should only be used directly inside HvlPainter
 	 * draw calls!
@@ -50,24 +57,26 @@ public final class HvlPaint {
 		else global.setValue(tArg);
 		return global;
 	}
-	
-//	/**
-//	 * Re-assigns the value of a global variable and returns that variable. This is an optimization 
-//	 * technique that removes the need for users to store their own HvlPaint instances and allows for rapid
-//	 * {@linkplain HvlPainter} draw calls with varying HvlPaint values.
-//	 * 
-//	 * NOTE: this references a volatile memory location and should only be used directly inside HvlPainter
-//	 * draw calls!
-//	 * 
-//	 * @param fArg the frame value assigned to the returned HvlPaint
-//	 * @return an instance of HvlPaint set to the specified value and mode
-//	 */
-//	public static HvlPaint hvlPaint(HvlRenderFrame fArg){
-//		if(global == null) global = new HvlPaint(fArg);
-//		else global.setValue(fArg);
-//		return global;
-//	}
-	
+
+	//	/**
+	//	 * Re-assigns the value of a global variable and returns that variable. This is an optimization 
+	//	 * technique that removes the need for users to store their own HvlPaint instances and allows for rapid
+	//	 * {@linkplain HvlPainter} draw calls with varying HvlPaint values.
+	//	 * 
+	//	 * <p>	
+	//	 * 
+	//	 * NOTE: this references a volatile memory location and should only be used directly inside HvlPainter
+	//	 * draw calls!
+	//	 * 
+	//	 * @param fArg the frame value assigned to the returned HvlPaint
+	//	 * @return an instance of HvlPaint set to the specified value and mode
+	//	 */
+	//	public static HvlPaint hvlPaint(HvlRenderFrame fArg){
+	//		if(global == null) global = new HvlPaint(fArg);
+	//		else global.setValue(fArg);
+	//		return global;
+	//	}
+
 	/**
 	 * The setting of a HvlPaint that determines the commands used to apply the paint to a polygon. This
 	 * can also be used to determine which variable in the HvlPaint instance is holding data.
@@ -75,85 +84,85 @@ public final class HvlPaint {
 	 * @author os_reboot
 	 *
 	 */
-	public static enum Mode{//TODO this visibility is broken
+	public enum HvlPaintMode{
 		COLOR, TEXTURE, RENDERFRAME
 	}
 
-	private Mode mode;
+	private HvlPaintMode mode;
 
 	private Color color;
 	private Texture texture;
-//	private HvlRenderFrame frame;
+	//	private HvlRenderFrame frame;
 
 	/**
-	 * Constructs a HvlPaint from a color. Automatically sets the mode to COLOR.
+	 * Constructs a HvlPaint from a color. Automatically sets the mode to <code>COLOR</code>.
 	 * 
 	 * @param cArg the value to assign to the 'color' variable
 	 */
 	HvlPaint(Color cArg){
 		color = cArg;
-		mode = Mode.COLOR;
+		mode = HvlPaintMode.COLOR;
 	}
 
 	/**
-	 * Constructs a HvlPaint from a texture. Automatically sets the mode to TEXTURE.
+	 * Constructs a HvlPaint from a texture. Automatically sets the mode to <code>TEXTURE</code>.
 	 * 
 	 * @param tArg the value to assign to the 'texture' variable
 	 */
 	HvlPaint(Texture tArg){
 		texture = tArg;
-		mode = Mode.TEXTURE;
+		mode = HvlPaintMode.TEXTURE;
 	}
-	
-//	/**
-//	 * Constructs a HvlPaint from a {@linkplain HvlRenderFrame}. Automatically sets the mode to RENDERFRAME.
-//	 * 
-//	 * @param fArg the value to assign to the 'frame' variable
-//	 */
-//	HvlPaint(HvlRenderFrame fArg){
-//		frame = fArg;
-//		mode = Mode.RENDERFRAME;
-//	}
-	
+
+	//	/**
+	//	 * Constructs a HvlPaint from a {@linkplain HvlRenderFrame}. Automatically sets the mode to <code>RENDERFRAME</code>.
+	//	 * 
+	//	 * @param fArg the value to assign to the 'frame' variable
+	//	 */
+	//	HvlPaint(HvlRenderFrame fArg){
+	//		frame = fArg;
+	//		mode = Mode.RENDERFRAME;
+	//	}
+
 	/**
 	 * @return the current mode of the HvlPaint
 	 */
-	public Mode getMode(){
+	public HvlPaintMode getMode(){
 		return mode;
 	}
-	
+
 	/**
 	 * @return the value of the 'color' variable
-	 * @throws WrongModeException if the mode does not match COLOR
+	 * @throws WrongModeException if the mode does not match <code>COLOR</code>
 	 */
 	public Color getValueColor(){
-		if(mode == Mode.COLOR)
+		if(mode == HvlPaintMode.COLOR)
 			return color;
-		else throw new WrongModeException(Mode.COLOR, mode);
+		else throw new WrongModeException(HvlPaintMode.COLOR, mode);
 	}
-	
+
 	/**
 	 * @return the value of the 'texture' variable
-	 * @throws WrongModeException if the mode does not match TEXTURE
+	 * @throws WrongModeException if the mode does not match <code>TEXTURE</code>
 	 */
 	public Texture getValueTexture(){
-		if(mode == Mode.TEXTURE)
+		if(mode == HvlPaintMode.TEXTURE)
 			return texture;
-		else throw new WrongModeException(Mode.TEXTURE, mode);
+		else throw new WrongModeException(HvlPaintMode.TEXTURE, mode);
 	}
-	
-//	/**
-//	 * @return the value of the 'frame' variable
-//	 * @throws WrongModeException if the mode does not match RENDERFRAME
-//	 */
-//	public HvlRenderFrame getValueRF(){
-//		if(mode == Mode.RENDERFRAME)
-//			return frame;
-//		else throw new WrongModeException(Mode.RENDERFRAME, mode);
-//	}
-	
+
+	//	/**
+	//	 * @return the value of the 'frame' variable
+	//	 * @throws WrongModeException if the mode does not match <code>RENDERFRAME</code>
+	//	 */
+	//	public HvlRenderFrame getValueRF(){
+	//		if(mode == Mode.RENDERFRAME)
+	//			return frame;
+	//		else throw new WrongModeException(Mode.RENDERFRAME, mode);
+	//	}
+
 	/**
-	 * Sets the value of the HvlPaint. Automatically sets mode to COLOR and sets non-color variables to
+	 * Sets the value of the HvlPaint. Automatically sets mode to <code>COLOR</code> and sets non-color variables to
 	 * null.
 	 * 
 	 * @param cArg the value to assign to the 'color' variable
@@ -161,12 +170,12 @@ public final class HvlPaint {
 	public void setValue(Color cArg){
 		color = cArg;
 		texture = null;
-//		frame = null;
-		mode = Mode.COLOR;
+		//		frame = null;
+		mode = HvlPaintMode.COLOR;
 	}
-	
+
 	/**
-	 * Sets the value of the HvlPaint. Automatically sets mode to TEXTURE and sets non-texture variables to
+	 * Sets the value of the HvlPaint. Automatically sets mode to <code>TEXTURE</code> and sets non-texture variables to
 	 * null.
 	 * 
 	 * @param tArg the value to assign to the 'texture' variable
@@ -174,27 +183,27 @@ public final class HvlPaint {
 	public void setValue(Texture tArg){
 		color = null;
 		texture = tArg;
-//		frame = null;
-		mode = Mode.TEXTURE;
+		//		frame = null;
+		mode = HvlPaintMode.TEXTURE;
 	}
-	
-//	/**
-//	 * Sets the value of the HvlPaint. Automatically sets mode to RENDERFRAME and sets non-HvlRenderFrame 
-//	 * variables to null.
-//	 * 
-//	 * @param fArg the value to assign to the 'frame' variable
-//	 */
-//	public void setValue(HvlRenderFrame fArg){
-//		color = null;
-//		texture = null;
-//		frame = fArg;
-//		mode = Mode.RENDERFRAME;
-//	}
+
+	//	/**
+	//	 * Sets the value of the HvlPaint. Automatically sets mode to <code>RENDERFRAME</code> and sets non-HvlRenderFrame 
+	//	 * variables to null.
+	//	 * 
+	//	 * @param fArg the value to assign to the 'frame' variable
+	//	 */
+	//	public void setValue(HvlRenderFrame fArg){
+	//		color = null;
+	//		texture = null;
+	//		frame = fArg;
+	//		mode = Mode.RENDERFRAME;
+	//	}
 
 	private static class WrongModeException extends RuntimeException{
 		private static final long serialVersionUID = -7821681025532438849L;
-		
-		private WrongModeException(Mode modeError, Mode modeCorrect){
+
+		private WrongModeException(HvlPaintMode modeError, HvlPaintMode modeCorrect){
 			super("Referenced the incorrect HvlPaint mode " + modeError.toString() + "! This HvlPaint's mode is " + modeCorrect.toString() + ".");
 		}
 	}
--- src/com/osreboot/ridhvl2/painter/HvlQuad.java
+++ src/com/osreboot/ridhvl2/painter/HvlQuad.java
@@ -1,5 +1,40 @@
 package com.osreboot.ridhvl2.painter;
 
-public final class HvlQuad {
-	
+import com.osreboot.ridhvl2.HvlCoord;
+
+public final class HvlQuad extends HvlPolygon{
+
+	private static HvlQuad global;
+
+	public static HvlQuad hvlQuad(float x, float y, float xl, float yl){
+		if(global == null){
+			global = new HvlQuad(
+					new HvlCoord(x, y), new HvlCoord(x + xl, y), 
+					new HvlCoord(x + xl, y + yl), new HvlCoord(x, y + yl)
+					);
+		}else{
+			global.setVertices(new HvlCoord[]{
+					new HvlCoord(x, y), new HvlCoord(x + xl, y), 
+					new HvlCoord(x + xl, y + yl), new HvlCoord(x, y + yl)
+			});
+		}
+		return global;
+	}
+
+	public static final HvlCoord[] COORDS_DEFAULT_UVS = new HvlCoord[]{
+			new HvlCoord(0, 0),
+			new HvlCoord(1, 0),
+			new HvlCoord(1, 1),
+			new HvlCoord(0, 1),
+	};
+
+	HvlQuad(HvlCoord c0Arg, HvlCoord c1Arg, HvlCoord c2Arg, HvlCoord c3Arg){
+		super(new HvlCoord[]{c0Arg, c1Arg, c2Arg, c3Arg}, COORDS_DEFAULT_UVS);
+	}
+
+	HvlQuad(HvlCoord c0Arg, HvlCoord c1Arg, HvlCoord c2Arg, HvlCoord c3Arg, 
+			HvlCoord uv0Arg, HvlCoord uv1Arg, HvlCoord uv2Arg, HvlCoord uv3Arg){
+		super(new HvlCoord[]{c0Arg, c1Arg, c2Arg, c3Arg}, new HvlCoord[]{uv0Arg, uv1Arg, uv2Arg, uv3Arg});
+	}
+
 }
