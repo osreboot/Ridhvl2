bf02a6b0718f573fa0932fa2222a1f6e090b2d07
osreboot <live2pwn@rocketmail.com>
2018-07-10 02:22:52 -0700

    Finished commenting HvlTimer

--- src/com/osreboot/ridhvl2/template/HvlTimer.java
+++ src/com/osreboot/ridhvl2/template/HvlTimer.java
@@ -17,12 +17,13 @@ public abstract class HvlTimer {
 	 * Common values used to represent the maximum possible delta (in case the program briefly becomes 
 	 * unresponsive).
 	 */
-	public static final long MAXDELTA_UNLIMITED = Long.MAX_VALUE,
-			MAXDELTA_SECOND = 1000,
-			MAXDELTA_DECISECOND = 100,
-			MAXDELTA_HALF_DECISECOND = 50,
-			MAXDELTA_CENTISECOND = 10,
-			MAXDELTA_MILLISECOND = 1;
+	public static final long 
+	MAXDELTA_UNLIMITED = Long.MAX_VALUE,
+	MAXDELTA_SECOND = 1000,
+	MAXDELTA_DECISECOND = 100,
+	MAXDELTA_HALF_DECISECOND = 50,
+	MAXDELTA_CENTISECOND = 10,
+	MAXDELTA_MILLISECOND = 1;
 
 	private float dilation = 1f, totalTimeS = 0f;
 	//totalTimeMS <- not affected by dilation
@@ -47,41 +48,76 @@ public abstract class HvlTimer {
 		}
 	}
 
+	/**
+	 * Called once per loop.
+	 * 
+	 * @param delta the time (in seconds) since the last <code>update</code> call
+	 */
 	public abstract void update(float delta);
 
+	/**
+	 * @return the number of <code>update</code> calls per second (predicted based on the last <code>delta</code> value)
+	 */
 	public float getUpdateRate(){
 		return 1f/((float)deltaMS / 1000);
 	}
 
+	/**
+	 * @return whether or not the loop is running
+	 */
 	public boolean isRunning(){
 		return running;
 	}
 
+	/**
+	 * Sets the loop's <code>running</code> value. The loop will not cycle again if this is set 
+	 * to false.
+	 * 
+	 * @param runningArg the new <code>running</code> value
+	 */
 	public void setRunning(boolean runningArg){
 		running = runningArg;
 	}
 
+	/**
+	 * @return the current <code>dilation</code> value of the loop
+	 */
 	public float getDilation(){
 		return dilation;
 	}
 
+	/**
+	 * Sets the loop's <code>dilation</code> value. This is the value by which <code>delta</code> is 
+	 * multiplied every cycle. This setting affects the <code>totalTime</code> of the loop. This is 
+	 * useful for creating bullet-time effects or similar.
+	 * 
+	 * @param dilationArg the new <code>dilation</code> value
+	 */
 	public void setDilation(float dilationArg){
 		dilation = dilationArg;
 	}
 
-	@Deprecated
-	public float getTotalTimeS() {
+	/**
+	 * @return the time (in seconds) since the loop started
+	 */
+	public float getTotalTime() {
 		return totalTimeS;
 	}
 
-	@Deprecated
+	/**
+	 * @return the maximum value (in milliseconds) that <code>delta</code> is restricted to
+	 */
 	public long getMaxDeltaMS(){
 		return maxDeltaMS;
 	}
 
-	@Deprecated
-	public void setMaxDeltaMS(long maxDeltaMSArg){
-		maxDeltaMS = maxDeltaMSArg;
+	/**
+	 * Sets the maximum value (in milliseconds) to restrict <code>delta</code> to.
+	 * 
+	 * @param maxDeltaArg the new maximum <code>delta</code> restriction
+	 */
+	public void setMaxDelta(long maxDeltaArg){
+		maxDeltaMS = maxDeltaArg;
 	}
 
 }
