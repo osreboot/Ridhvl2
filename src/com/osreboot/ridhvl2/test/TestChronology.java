e046490edd26b6c4ead592ecf6a733c296334def
osreboot <calvin.16.weaver@gmail.com>
2018-07-19 04:00:31 -0700

    Converted TestChronology to lambda expressions

--- src/com/osreboot/ridhvl2/test/TestChronology.java
+++ src/com/osreboot/ridhvl2/test/TestChronology.java
@@ -26,19 +26,13 @@ class TestChronology {
 		public static final int LAUNCH_CODE_RAW = 1, LAUNCH_CODE = (int)Math.pow(2, LAUNCH_CODE_RAW);
 
 		@HvlChronologyInitialize(label = "TestChrono1", chronology = HvlChronology.CHRONOLOGY_INIT_MIDDLE, launchCode = LAUNCH_CODE_RAW)
-		public static final HvlAction.A1<Boolean> ACTION_INIT = new HvlAction.A1<Boolean>(){
-			@Override
-			public void run(Boolean debug) {
-				HvlLogger.println(debug, TestChronology.class, "1 - Initialized!");
-			}
+		public static final HvlAction.A1<Boolean> ACTION_INIT = (debug) -> {
+			HvlLogger.println(debug, TestChronology.class, "1 - Initialized!");
 		};
 
 		@HvlChronologyUpdate(label = "TestChrono1", chronology = HvlChronology.CHRONOLOGY_UPDATE_PRE_MIDDLE, launchCode = LAUNCH_CODE_RAW)
-		public static final HvlAction.A2<Boolean, Float> ACTION_UPDATE = new HvlAction.A2<Boolean, Float>() {
-			@Override
-			public void run(Boolean debug, Float delta) {
-				HvlLogger.println(debug, TestChronology.class, "1 - Updated!");
-			}
+		public static final HvlAction.A2<Boolean, Float> ACTION_UPDATE = (debug, delta) -> {
+			HvlLogger.println(debug, TestChronology.class, "1 - Updated!");
 		};
 
 	}
@@ -48,19 +42,13 @@ class TestChronology {
 		public static final int LAUNCH_CODE_RAW = 2, LAUNCH_CODE = (int)Math.pow(2, LAUNCH_CODE_RAW);
 
 		@HvlChronologyInitialize(label = "TestChrono2", chronology = HvlChronology.CHRONOLOGY_INIT_MIDDLE + 1, launchCode = LAUNCH_CODE_RAW)
-		public static final HvlAction.A1<Boolean> ACTION_INIT = new HvlAction.A1<Boolean>(){
-			@Override
-			public void run(Boolean debug) {
-				HvlLogger.println(debug, TestChronology.class, "2 - Initialized!");
-			}
+		public static final HvlAction.A1<Boolean> ACTION_INIT = debug -> {
+			HvlLogger.println(debug, TestChronology.class, "2 - Initialized!");
 		};
 
 		@HvlChronologyUpdate(label = "TestChrono2", chronology = HvlChronology.CHRONOLOGY_UPDATE_PRE_MIDDLE - 1, launchCode = LAUNCH_CODE_RAW)
-		public static final HvlAction.A2<Boolean, Float> ACTION_UPDATE = new HvlAction.A2<Boolean, Float>() {
-			@Override
-			public void run(Boolean debug, Float delta) {
-				HvlLogger.println(debug, TestChronology.class, "2 - Updated!");
-			}
+		public static final HvlAction.A2<Boolean, Float> ACTION_UPDATE = (debug, delta) -> {
+			HvlLogger.println(debug, TestChronology.class, "2 - Updated!");
 		};
 
 	}
@@ -70,19 +58,13 @@ class TestChronology {
 		public static final int LAUNCH_CODE_RAW = 3, LAUNCH_CODE = (int)Math.pow(2, LAUNCH_CODE_RAW);
 
 		@HvlChronologyInitialize(label = "TestChronoDuplicate", chronology = HvlChronology.CHRONOLOGY_INIT_MIDDLE, launchCode = LAUNCH_CODE_RAW)
-		public static final HvlAction.A1<Boolean> ACTION_INIT = new HvlAction.A1<Boolean>(){
-			@Override
-			public void run(Boolean debug) {
-				HvlLogger.println(debug, TestChronology.class, "DUPE - Initialized!");
-			}
+		public static final HvlAction.A1<Boolean> ACTION_INIT = debug -> {
+			HvlLogger.println(debug, TestChronology.class, "DUPE - Initialized!");
 		};
 
 		@HvlChronologyUpdate(label = "TestChronoDuplicate", chronology = HvlChronology.CHRONOLOGY_UPDATE_PRE_MIDDLE, launchCode = LAUNCH_CODE_RAW)
-		public static final HvlAction.A2<Boolean, Float> ACTION_UPDATE = new HvlAction.A2<Boolean, Float>() {
-			@Override
-			public void run(Boolean debug, Float delta) {
-				HvlLogger.println(debug, TestChronology.class, "DUPE - Updated!");
-			}
+		public static final HvlAction.A2<Boolean, Float> ACTION_UPDATE = (debug, delta) -> {
+			HvlLogger.println(debug, TestChronology.class, "DUPE - Updated!");
 		};
 
 	}
